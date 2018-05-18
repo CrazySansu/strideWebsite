000918df2ada579f32c09db212e5b7e2203881db
Your Name <you@example.com>
2018-05-19 04:08:04 +0530

    Updated 19th May 04:07 am

--- js/app.js
+++ js/app.js
@@ -1,72 +1,126 @@
-
-
-
-
-
-
-// --------------------------------------------------------------------------
-
-function showButtons(name){
-        var btn = document.querySelector('#'+name);
-        // console.log(btn);
-        var buttonWrapper = document.querySelectorAll('.'+name);
-        if(btn.classList.contains('animation')) {
-          btn.classList.remove('animation');
-        } else {
-          btn.classList.add('animation');
+$(function () {
+    $("#navbarToggle").blur(function (event){
+        var screenWidth = window.innerWidth;
+        if(screenWidth <768){
+            $("#collapsable-nav").collapse('hide');
+            $('.content').css("margin-top",90);
         }
+    });
     
-        for (i = 0; i < buttonWrapper.length; i++ ) {
-          if(buttonWrapper[i].classList.contains('animation')){
-            buttonWrapper[i].classList.remove('animation');   
-          } else {
-            buttonWrapper[i].classList.add('animation');
-          }  
-        }
-      };
-
-let prism = document.querySelector(".rec-prism");
-
-function showSignup(){
-  prism.style.transform = "translateZ(-100px) rotateY( -90deg)";
-}
-function showLogin(){
-  prism.style.transform = "translateZ(-100px)";
-}
-function showForgotPassword(){
-  prism.style.transform = "translateZ(-100px) rotateY( -180deg)";
-}
-
-function showSubscribe(){
-  prism.style.transform = "translateZ(-100px) rotateX( -90deg)";
-}
-
-function showContactUs(){
-  prism.style.transform = "translateZ(-100px) rotateY( 90deg)";
-}
-
-function showThankYou(){
-  prism.style.transform = "translateZ(-100px) rotateX( 90deg)";
-}
-
-(function () {
-    'use strict';
-    var strideAI = angular.module('strideAI', []);
-    window.onload = function () {
-      // body...
-    }
-  
-    strideAI.controller('navbarTab',function($scope){
-        // console.log($scope.white); 
+    $("#navbarToggle").click(function (event){
+        $('.content').css("margin-top",350);
     });
 
-    window.onscroll = function() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            // document.getElementById("headingTag").style.background = "linear-gradient(to right, #ffffff -50%,  #BA25FF 50%)";
-            // document.getElementById("white").src = "img/logo-color.png";
-        } else {
-            // document.getElementById("headingTag").style.background = "linear-gradient(to right, #2570ff -50%,  #BA25FF 50%)";
-            // document.getElementById("white").src = "img/logo-white.png";
+    $('.dropdown').hover(
+        function () {
+            $(this).toggleClass('open');
+        },
+        function() {
+            $(this).toggleClass('open');
         }
+    );
+});
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("headingTag").style.background = "linear-gradient(to right, #ffffff -50%,  #BA25FF 50%)";
+        // document.getElementById("headingTag").style.background = "black";
+        document.getElementById("logo-white").style.display = "none";
+        document.getElementById("logo-color").style.display = "block";
+    } else {
+        document.getElementById("headingTag").style.background = "linear-gradient(to right, #2570ff -50%,  #BA25FF 50%)";
+        document.getElementById("logo-white").style.display = "block";
+        document.getElementById("logo-color").style.display = "none";
     }
-})();
+}
+
+
+// --------- Platform Animation
+
+var nluTool = document.getElementById('nluTool').addEventListener('click',showNLU);
+
+
+function showNLU(){
+    // this.style.transform = "scale(1.5)";
+    document.getElementById('nlu').style.display = "block";
+    document.getElementById('nlu').style.transform = "scale(1)";
+}
+
+
+// ---------- Animation for the NLU Prism
+
+let prism = document.querySelector(".recPrism");
+console.log(prism);
+
+function showSubjectiveSA(){
+    prism.style.transform = "translateZ(-150px) rotateY( -90deg)";
+}
+function showSentimentAnalysis(){
+    prism.style.transform = "translateZ(-150px)";
+}
+function showTextSummarization(){
+    prism.style.transform = "translateZ(-150px) rotateY( -180deg)";
+}
+
+function showKeywordExtraction(){
+    prism.style.transform = "translateZ(-150px) rotateX( -90deg)";
+}
+
+function showSemanticSimilarity(){
+    prism.style.transform = "translateZ(-150px) rotateY( 90deg)";
+}
+
+function showBottomSide(){
+    prism.style.transform = "translateZ(-150px) rotateX( 90deg)";
+}
+
+
+// ---------- Animation js code for contact coming out from pic
+
+document.querySelector("#vj").addEventListener('click', showButtons);
+document.querySelector("#sandy").addEventListener('click', showButtons);
+document.querySelector("#prat").addEventListener('click', showButtons);
+document.querySelector("#sagar").addEventListener('click', showButtons);
+
+document.querySelector("#vj").addEventListener('blur', hideButtons);
+document.querySelector("#sandy").addEventListener('blur', hideButtons);
+document.querySelector("#prat").addEventListener('blur', hideButtons);
+document.querySelector("#sagar").addEventListener('blur', hideButtons);
+
+
+function showButtons(){
+    var btnWrapper = this.parentElement.querySelectorAll('.button-inside');
+    // console.log(this);
+    
+    if(this.classList.contains('animation')) {
+        this.classList.remove('animation');
+    } else {
+        this.classList.add('animation');
+    }
+
+    for (i = 0; i < btnWrapper.length; i++ ) {
+        if(btnWrapper[i].classList.contains('animation')){
+            btnWrapper[i].classList.remove('animation');   
+        } else {
+            btnWrapper[i].classList.add('animation');
+        } 
+    }
+}
+
+function hideButtons(){
+    var btnWrapper = this.parentElement.querySelectorAll('.button-inside');
+    this.classList.remove('animation');
+
+    for (i = 0; i < btnWrapper.length; i++ ) {
+        btnWrapper[i].classList.remove('animation'); 
+    }
+}
+
+// (function(){
+//     'use strict';
+//     var app = angular.module('strideApp',[]);
+//     app.controller('name',function($scope){         //ng-controller='name' in html
+
+//     });
+// })();
+
